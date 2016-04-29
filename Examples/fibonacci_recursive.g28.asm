49d4fae806d106409daefae10dc7c79f7fceb20e
Snehal Shendware <snehalshendware1@gmail.com>
2016-04-29 16:43:57 -0700

    Changed Examples for recursive

---
+++ Examples/fibonacci_recursive.g28.asm
@@ -0,0 +1,40 @@
+fth
+put a
+fun fibo
+pta val
+get val
+get 2
+lst
+bne 10
+jbk val
+get 1
+beq 32
+get val
+get 1
+sub
+put x
+get val
+get 2
+sub
+put y
+gta x
+get fibo
+run
+put m
+gta y
+get fibo
+run
+put n
+get m
+get n
+add
+put p
+jbk p
+fnd
+gta a
+get fibo
+run
+put c
+get c
+dsp
+end
