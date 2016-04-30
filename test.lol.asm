889e5a377eeecd52b9d002f37e07d2b1180df2fd
Abir Saha <abir.saha@asu.edu>
2016-04-29 18:39:19 -0700

    Test

---
+++ test.lol.asm
@@ -0,0 +1,29 @@
+stk a
+get 1
+psh a
+get 5
+psh a
+get 8
+psh a
+str "Inserted_1,5,8"
+dsp
+pek a
+put b
+str "element_at_the_top_of_stack_is"
+dsp
+f_stack_is"
+dsp
+get b
+dsp
+pop a
+str "Removed_element_from_the_top_is"
+dsp
+_the_top_is"
+dsp
+pek a
+put d
+str "New_top_is"
+dsp
+get d
+dsp
+end
