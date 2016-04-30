5cec9bc4aa31acc775e3fe6173331ee06785d838
Snehal Shendware <snehalshendware1@gmail.com>
2016-04-29 17:09:37 -0700

    Iterative factorial code

---
+++ Examples/factorial_iterative.g28.asm
@@ -0,0 +1,25 @@
+fth
+put a
+get 1
+put product
+get a
+get 1
+grt
+bne 17
+get product
+get a
+mul
+put product
+get a
+get 1
+sub
+put a
+get 1
+beq 4
+str "The_factorial_result_is"
+dsp
+_is"
+dsp
+get product
+dsp
+end
