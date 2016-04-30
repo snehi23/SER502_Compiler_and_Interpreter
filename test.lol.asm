c63e214b56f975520f92925ebc1952ca84fad26b
Abir Saha <abir.saha@asu.edu>
2016-04-29 18:55:57 -0700

    edited

--- test.lol.asm
+++ test.lol.asm
@@ -5,25 +5,16 @@ get 5
 psh a
 get 8
 psh a
-str "Inserted_1,5,8"
-dsp
 pek a
 put b
-str "element_at_the_top_of_stack_is"
-dsp
-f_stack_is"
-dsp
 get b
 dsp
 pop a
-str "Removed_element_from_the_top_is"
-dsp
-_the_top_is"
+put c
+get c
 dsp
 pek a
 put d
-str "New_top_is"
-dsp
 get d
 dsp
 end
